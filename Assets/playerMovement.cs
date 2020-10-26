51f1240c7f3802e7ff3a70f2b02254b0f8282729
MD.Yeasin Sheikh <46500228+yeasin50@users.noreply.github.com>
2020-10-26 16:42:05 +0600

    player movement

---
+++ Assets/playerMovement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+
+    public CharacterController controller ;
+    public float speed = 14f;
+    
+    public float  gravity= -9.81f;
+    public float groundDistance = 0.4f;
+    public float jumpHeight = 3f;
+
+    public Transform groundCheck;
+    
+    public LayerMask  groundMask;
+
+    Vector3 velocity ;
+    bool isGounded;
+
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if(isGounded && velocity.y <0){
+            velocity.y = -2f;
+        }
+
+        float x =  Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move =  transform.right * x + transform.forward*z ;
+        controller.Move(move * speed *Time.deltaTime);
+        
+        if(Input.GetButtonDown("Jump") && isGounded){
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity );
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+        //for physics y = .5*gt^2
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
