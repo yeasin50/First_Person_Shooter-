4b08435a939a99d9406a41d6fba02ca61c95833e
MD.Yeasin Sheikh <46500228+yeasin50@users.noreply.github.com>
2020-10-27 19:57:34 +0600

    gun fire

---
+++ Assets/Target_.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target_ : MonoBehaviour
+{
+    public float health = 50f;
+
+    public void TakeDamage(float amount){
+        health -=amount;
+
+        if(health<=0){
+            DIE();
+        }
+    }
+    void DIE(){
+        Destroy(gameObject);
+    }
+}
