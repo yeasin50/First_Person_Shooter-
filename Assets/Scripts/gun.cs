266ba2d9736cfdce5d10ce880e159919848b3892
MD.Yeasin Sheikh <46500228+yeasin50@users.noreply.github.com>
2020-10-29 13:08:15 +0600

    muzzleFlesh+ FireRate

--- Assets/Scripts/gun.cs
+++ Assets/Scripts/gun.cs
@@ -5,18 +5,26 @@ public class gun : MonoBehaviour
 {
     public float damage = 10f;
     public float range = 100f;
-
+    public float fireRate = 15f;
     public Camera fpsCam;
+    public ParticleSystem muzzleFlesh;
+    public GameObject impactEffects;
 
+
+    private float nextTimeToFire = 0f;
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetButtonDown("Fire1")){
+        if(Input.GetButton("Fire1") && Time.time >= nextTimeToFire){
+            nextTimeToFire = Time.time + 1f/fireRate;
             Shoot();
         }
     }
 
     void Shoot(){
+
+        muzzleFlesh.Play();
+
         RaycastHit hit ;
         //hit something or not
         if(Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range )){
@@ -26,6 +34,9 @@ public class gun : MonoBehaviour
             if(target!=null){
                 target.TakeDamage(damage);
             }
+
+           GameObject impactE =  Instantiate(impactEffects,hit.point, Quaternion.LookRotation(hit.normal));
+            Destroy(impactE,2f);
         }
     }
 }
