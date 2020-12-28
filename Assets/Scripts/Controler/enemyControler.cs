89d7a114b5087dc6893cf056c36d9e9b89e76ffd
MD.Yeasin Sheikh <46500228+yeasin50@users.noreply.github.com>
2020-12-28 12:23:10 +0600

    'EnemyAI'

---
+++ Assets/Scripts/Controler/enemyControler.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class enemyControler : MonoBehaviour
+{
+
+    public float lookRadius = 4f;
+
+    Transform traget;
+    NavMeshAgent agent;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        traget = playerManager1.instance.player.transform;
+        agent = GetComponent<NavMeshAgent>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = Vector3.Distance(traget.position, transform.position);
+        
+        if(distance<=lookRadius){
+            agent.SetDestination(traget.position);
+            Debug.Log("In Range");
+        }
+    }
+
+      // debug
+    private void OnDrawGizmosSelected() {
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, lookRadius);
+    }
+}
---
+++ Assets/Scripts/Controler/playerManager1.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerManager1 : MonoBehaviour
+{
+   #region Singleton
+   public static playerManager1 instance;
+
+   private void Awake() {
+       instance = this;
+   }
+   #endregion 
+   public GameObject player;
+}
